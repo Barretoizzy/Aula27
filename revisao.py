59a972fa46bcf200aefce7328c73dc14a3eba9a5
izabele <izabele.matias@aluno.senai.br>
2024-12-02 10:09:25 -0500

    revisao

---
+++ revisao.py
@@ -0,0 +1,13 @@
+#ler entradas do usuário
+#nome = (input) #armazenar o nome do aluno 
+#print(nome) #armazenar o nome do aluno
+nota1 = float (input())
+nota2 = float (input())
+nota3 = float (input())
+nota4 = float (input())
+
+#faltas = int (input())
+
+#calculo da mèdia
+media = (nota1+nota2+nota3+nota4)/4
+print(media)
