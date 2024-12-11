a55f236bc5e6e099eed827e075461c3ec5e7cb37
izabele <izabele.matias@aluno.senai.br>
2024-12-11 08:47:11 -0500

    usando função

---
+++ funcao.py
@@ -0,0 +1,14 @@
+# declarar uma função
+def bom_dia():
+    print("BUENOS DIAS!")
+
+
+def boa_noite():
+    print("BOA NOITE!")
+
+def boa_tarde():
+    print("BUENAS TARDE")
+
+bom_dia()
+boa_tarde()
+boa_noite()
